30a6243d1279b17f61234ae011c2ee4a92c9c806
dsantial <gaoxd126@gmail.com>
2023-01-04 15:53:48 +0800

    Update parseUrlResponse.py

--- parseUrlResponse.py
+++ parseUrlResponse.py
@@ -28,6 +28,8 @@ def xpath_for_parse(response):
         print(title)
 
 def re_for_parse(response):
+    # <div class="name"><a href="http://product.dangdang.com/29122655.html" target="_blank" title="少年读徐霞客游记（游中国·地理启蒙，全3册；读万卷书，行万里路！当代著名地质学家、科普作家刘兴诗与古代伟大的旅行家、地理学家徐霞客穿越时空，神奇相遇，趣味解读多姿多彩的中国地理！）">少年读徐霞客游记（游中国·地理启蒙，全3册；读万卷书，行万里路<span class="dot">...</span></a>
+    # 匹配上面的字符串,\d+ 匹配多个数字,(.*?)非贪婪匹配任意字符
     reg = '<div class="name"><a href="http://product.dangdang.com/\d+.html" target="_blank" title="(.*?)">'
     for title in re.findall(reg, response):
         print(title)
